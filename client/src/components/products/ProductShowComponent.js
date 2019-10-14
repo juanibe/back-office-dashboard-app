bf520d1629173295b4be2847b65eed9d8e9be696
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2019-10-15 01:17:33 +0200

    - Implemented delete functionality to detail view

--- client/src/components/products/ProductShowComponent.js
+++ client/src/components/products/ProductShowComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { withRouter } from 'react-router-dom';
-import ProductDeleteItemComponent from '../../components/products/ProductDeleteItemComponent'
-import Axios from "axios";
+import SweetAlert from 'react-bootstrap-sweetalert';
+import axios from "axios";
 import { getJwt } from '../../helpers/jwt'
 import back from '../../img/return.png'
 import bin from '../../img/bin.png'
@@ -17,17 +17,26 @@ class ProductShowComponent extends Component {
     }
   }
 
+  deleteConfirmation = () => {
+    const jwt = getJwt()
+    axios.delete(`http://localhost:3001/api/v1/products/${this.state.product_id}`, { headers: { 'Authorization': `Bearer ${jwt}` } })
+      .then(response => {
+        this.setState({ alert: false })
+        this.props.history.push('/products')
+      })
+  }
+
   componentDidMount() {
     const jwt = getJwt()
-    Axios.get(`http://localhost:3001/api/v1/products/${this.state.product_id}`, { headers: { 'Authorization': `Bearer ${jwt}` } })
+    axios.get(`http://localhost:3001/api/v1/products/${this.state.product_id}`, { headers: { 'Authorization': `Bearer ${jwt}` } })
       .then(response => {
+        console.log('product', response)
         this.setState({ product: response.data })
       })
   }
 
 
   render() {
-    console.log(this.state.product)
     if (!this.state.product) {
       return (
         <div>Loading...</div>
@@ -35,6 +44,20 @@ class ProductShowComponent extends Component {
     }
     return (
       <div className="main-content">
+        {this.state.alert && (
+          <SweetAlert
+            warning
+            showCancel
+            confirmBtnText="Yes, delete it!"
+            confirmBtnBsStyle="danger"
+            cancelBtnBsStyle="default"
+            title="Are you sure?"
+            onConfirm={() => { this.deleteConfirmation() }}
+            onCancel={() => { this.setState({ alert: false }) }}
+          >
+            You will delete this product premanently...
+          </SweetAlert>
+        )}
         <div class="card text-center">
           <div class="card-header">
             Featured
@@ -50,7 +73,7 @@ class ProductShowComponent extends Component {
             </div>
             <button className="btn btn-light" onClick={this.props.history.goBack}><img src={back} /></button>
             <button className="btn btn-light" onClick={this.props.history.goBack}><img src={writing} /></button>
-            <ProductDeleteItemComponent />
+            <span><button className="btn btn-light" onClick={() => this.setState({ alert: true })}><img src={bin} /></button></span>
           </div>
           <div class="card-footer text-muted">
             2 days ago
