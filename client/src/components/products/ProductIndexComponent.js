69c321ff7b048a121f8c96514a8144b0e0b09769
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2019-10-15 01:16:50 +0200

    - Minor changes

--- client/src/components/products/ProductIndexComponent.js
+++ client/src/components/products/ProductIndexComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Link, withRouter } from 'react-router-dom'
 import ReactTable from 'react-table'
-import ProductDeleteItemComponent from '../../components/products/ProductDeleteItemComponent'
 import { getJwt } from '../../helpers/jwt'
 import axios from 'axios'
 import eye from '../../img/eye.png'
