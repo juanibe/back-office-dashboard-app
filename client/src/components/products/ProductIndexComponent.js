7bedcfd8fb7d1088b0a9c769f36c14f4b2af89b0
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2019-10-15 00:49:49 +0200

    - Fixed delete functionality

--- client/src/components/products/ProductIndexComponent.js
+++ client/src/components/products/ProductIndexComponent.js
@@ -9,6 +9,8 @@ import writing from '../../img/writing.png'
 import plus from '../../img/plus.png'
 import available_icon from '../../img/available.png'
 import not_available_icon from '../../img/not_available.png'
+import SweetAlert from 'react-bootstrap-sweetalert';
+import bin from '../../../src/img/bin.png'
 
 
 class ProductIndexComponent extends Component {
@@ -16,17 +18,27 @@ class ProductIndexComponent extends Component {
     super(props)
     this.state = {
       data: [],
+      product: null,
       categories: [],
       category_value: '',
       availability_value: '',
       filters: {},
       pages: null,
-      alert: null,
+      alert: false,
     }
   }
 
-  handleChange = (value) => {
-    this.setState({ data: value })
+  deleteConfirmation = () => {
+    const jwt = getJwt()
+    axios.delete(`http://localhost:3001/api/v1/products/${this.state.product}`, { headers: { 'Authorization': `Bearer ${jwt}` } })
+      .then(response => {
+        let data = this.state.data.filter(a => { return a._id !== response.data.id })
+        console.log(data)
+        this.setState({
+          alert: false,
+          data: data
+        })
+      })
   }
 
   applyFilter = (value) => {
@@ -55,7 +67,6 @@ class ProductIndexComponent extends Component {
 
     axios.get(`http://localhost:3001/api/v1/products`, config)
       .then(response => {
-        console.log(response.config.params)
         this.setState({
           data: response.data.result
         })
@@ -94,6 +105,22 @@ class ProductIndexComponent extends Component {
     }
     return (
       <div className='main-content'>
+        <div>
+          {this.state.alert && (
+            <SweetAlert
+              warning
+              showCancel
+              confirmBtnText="Yes, delete it!"
+              confirmBtnBsStyle="danger"
+              cancelBtnBsStyle="default"
+              title="Are you sure?"
+              onConfirm={() => { this.deleteConfirmation() }}
+              onCancel={() => { this.setState({ alert: false }) }}
+            >
+              You will delete this product premanently...
+          </SweetAlert>
+          )}
+        </div>
         <div>
           <Link to={'/products/add'}><button className="btn btn-light"><img src={plus} /></button></Link>
         </div>
@@ -148,8 +175,7 @@ class ProductIndexComponent extends Component {
                   Cell: row => [
                     <Link to={`products/${row.original._id}/show`} params={{ id: row.original._id }}><button className="btn btn-light"><img src={eye} /></button></Link>,
                     <Link to={`products/${row.original._id}/edit`}><button className="btn btn-light"><img src={writing} /></button></Link>,
-                    <ProductDeleteItemComponent product={row.original._id} products={this.state.data} handleChange={this.handleChange} />
-                  ]
+                    <span><button className="btn btn-light" onClick={() => this.setState({ alert: true, product: row.original._id })}><img src={bin} /></button></span>]
                 }
               ]
             }
