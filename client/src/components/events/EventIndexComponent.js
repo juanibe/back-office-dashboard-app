d4404e8883244d02e9c72541bd79ae2cbbc03cca
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2019-12-19 22:16:23 +0000

    - Added filter functionality to event index view

--- client/src/components/events/EventIndexComponent.js
+++ client/src/components/events/EventIndexComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CustomReactTable from "../CustomReactTable";
 import { withRouter } from 'react-router-dom';
 import { getJwt } from '../../helpers/jwt'
+import axios from "axios";
 import check from '../../img/check.png'
 import uncheck from '../../img/uncheck.png'
 
@@ -9,11 +10,27 @@ class EventIndexComponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      clients: [],
       columns: [
         {
+          id: 'client',
           Header: 'Client',
-          accessor: 'client[0].full_name',
+          accessor: 'client',
+          Cell: row => {
+            return <div>{row.original.client[0].full_name}</div>
+          },
+
           filterable: true,
+          id: 'client',
+           Filter: ({ filter, onChange }) =>
+             <select style={{ width: "100%", height: "100%" }} onChange={event => onChange(event.target.value)} value={filter ? filter.value : undefined} >
+          //     <option value={""}>Show All</option>
+               {this.state.clients.map((client) => {
+                return (
+                  <option key={client._id} value={client._id}>{client.full_name}</option>
+                )
+              })}
+            </select>
         },
         {
           Header: 'Place',
@@ -39,12 +56,16 @@ class EventIndexComponent extends Component {
     if (!jwt) {
       this.props.history.push('/login')
     }
+    axios.get('http://localhost:3001/api/v1/clients', { headers: { 'Authorization': `Bearer ${jwt}` } })
+      .then(response => {
+        this.setState({ clients: response.data.result })
+      })
   }
 
   render() {
     return (
       <div className='main-content'>
-        <CustomReactTable columns={this.state.columns} modelName={"Events"} />
+        <CustomReactTable columns={this.state.columns} modelName={"Events"} user={this.props.user} />
       </div >
     )
   }
