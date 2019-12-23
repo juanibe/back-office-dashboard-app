e3fcf3e1741aaee668350c4d0587aabedb7345b5
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2019-12-23 23:55:13 +0000

    - Created user routes

--- server/routes/api-routes/user.js
+++ server/routes/api-routes/user.js
@@ -2,8 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const auth_controller = require('../../controllers/authController');
+const user_controller = require('../../controllers/userController');
+
 
 router.get('/get-user', auth_controller.getUser);
+
+router.get('/count-documents', user_controller.countDocuments);
+router.get('/', user_controller.index)
+router.get('/:id', user_controller.show)
+router.put('/:id', user_controller.update)
+router.delete('/:id', user_controller.delete)
+
+
 
 module.exports = router;
 
