6e772c03a487b616e2b3b3bc9662628d11903dbb
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2019-12-24 00:12:42 +0000

    - Added routes to css files

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../src/css/react-table.css'
+import '../src/css/show-view.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css'
