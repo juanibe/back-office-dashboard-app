b34f263db50b87e63d59742b94c0f9219bea5919
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2020-02-02 21:12:57 +0000

    - Deleted getAllModels method

--- server/routes/api-routes/general.js
+++ server/routes/api-routes/general.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-
 const generalController = require("../../controllers/generalController");
 
-router.get('/', generalController.getAllModels);
 router.get('/events-day', generalController.listEventsByDate);
 router.get('/group-date', generalController.groupByDate);
 router.get('/availability', generalController.checkDateAvailability);
